c9023f48513d0d6178b7da9d8990e2926471c78c
Anastasia Filyushkina <iceqx521@mail.ru>
2018-04-25 22:09:44 +0300

    ,

--- MathLib/MathTests/BubbleSortTests.cs
+++ MathLib/MathTests/BubbleSortTests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using ClassLibrary1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace MathTests
 {
@@ -7,13 +8,66 @@ namespace MathTests
     public class BubbleSortTests
     {
         [TestMethod]
-        public void Sort_Puz1()
+        public void Sort_Puz8()
         {
             //arrange
-            
-            //act
+            BubbleSort bubble = new BubbleSort();
 
+            Random n = new Random();
+            int[] mas = new int[1000];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = n.Next(1, 1000);
+            }
+            //act
+            mas = bubble.DoBubbleSort(mas);
             //assert
+            Assert.IsTrue(Check(mas));
+        }
+        public bool Check(int[] mas)
+        {
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                if (mas[i] > mas[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        [TestMethod]
+        public void Sort_Puz9()
+        {
+            //arrange
+            BubbleSort bubble = new BubbleSort();
+
+            Random n = new Random();
+            int[] mas = new int[10000];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = n.Next(1, 10000);
+            }
+            //act
+            mas = bubble.DoBubbleSort(mas);
+            //assert
+            Assert.IsTrue(Check(mas));
+        }
+        [TestMethod]
+        public void Sort_Puz10()
+        {
+            //arrange
+            BubbleSort bubble = new BubbleSort();
+
+            Random n = new Random();
+            int[] mas = new int[100000];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = n.Next(1, 100000);
+            }
+            //act
+            mas = bubble.DoBubbleSort(mas);
+            //assert
+            Assert.IsTrue(Check(mas));
         }
     }
 }
