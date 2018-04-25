73f1fb679fae84f3a26eee6d9a57df153a1b7d8a
DemiK0r <demikor97@gmail.com>
2018-04-25 23:38:40 +0300

    Написал название методов

--- MathLib/MathTests/BubbleSortTests.cs
+++ MathLib/MathTests/BubbleSortTests.cs
@@ -8,17 +8,6 @@ namespace MathTests
     [TestClass]
     public class BubbleSortTests
     {
-        private static BubbleSort bubbleSort = new BubbleSort();
-
-        [TestMethod]
-        public void Sort_Puz1()
-        {
-            //arrange
-            
-            //act
-
-            //assert
-        }
 
         [TestMethod]
         public void Sort_Puz5()
