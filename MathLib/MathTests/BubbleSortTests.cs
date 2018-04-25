3933055c0192d4d1a14c218bd7b6316ffe26ff8e
DemiK0r <demikor97@gmail.com>
2018-04-25 23:36:41 +0300

    Ничего не сделал

--- MathLib/MathTests/BubbleSortTests.cs
+++ MathLib/MathTests/BubbleSortTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using ClassLibrary1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MathTests
@@ -6,11 +8,43 @@ namespace MathTests
     [TestClass]
     public class BubbleSortTests
     {
+        private static BubbleSort bubbleSort = new BubbleSort();
+
         [TestMethod]
         public void Sort_Puz1()
         {
             //arrange
             
+            //act
+
+            //assert
+        }
+
+        [TestMethod]
+        public void Sort_Puz5()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+
+        [TestMethod]
+        public void Sort_Puz6()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+
+        [TestMethod]
+        public void Sort_Puz7()
+        {
+            //arrange
+
             //act
 
             //assert
