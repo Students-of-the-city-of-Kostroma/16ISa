434c744608d255aafff689beea7e001a8d702acb
Владислав Котельников <kiselek44@gmail.com>
2018-05-04 19:38:58 +0300

    Добавлены основные интерфейсы в библиотеку

---
+++ MathLibApp/MathLibDLL/Interfaces/IExpression.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLibDLL.Models;
+
+namespace MathLibDLL.Interfaces
+{
+    /// <summary>
+    /// Математическое выражение используемое для вычислений
+    /// </summary>
+    public interface IExpression
+    {
+        /// <summary>
+        /// Строка математического выражения
+        /// </summary>
+        string Expression { set; get; }
+
+        /// <summary>
+        /// Аргументы используемые в выражении
+        /// </summary>
+        List<Argument> Arguments { get; }
+
+        /// <summary>
+        /// Добавить новый аргумент
+        /// </summary>
+        /// <param name="argument">Аргумент</param>
+        void AddArgument(Argument argument);
+
+        /// <summary>
+        /// Добавить новый аргумент
+        /// </summary>
+        /// <param name="name">Имя аргумента</param>
+        /// <param name="value">Значения аргумента</param>
+        void AddArgument(string name, double value);
+
+        /// <summary>
+        /// Измениить значение аргумента
+        /// </summary>
+        /// <param name="name">Имя аргумента</param>
+        /// <param name="value">Новое значение аргумента</param>
+        void EditArgument(string name, double value);
+
+        /// <summary>
+        /// Удалить аргумент из списка
+        /// </summary>
+        /// <param name="name">Имя аргумента</param>
+        void RemoveArgument(string name);
+
+        /// <summary>
+        /// Удалить аргумент из списка
+        /// </summary>
+        /// <param name="argument">Аргумент</param>
+        void RemoveArgument(Argument argument);
+
+        /// <summary>
+        /// Получить значение аргумента по имени
+        /// </summary>
+        /// <param name="name">Имя аргумента</param>
+        /// <returns>Значние</returns>
+        double GetArgumentValue(string name);
+    }
+}
---
+++ MathLibApp/MathLibDLL/Interfaces/IMathCore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLibDLL.Models;
+
+namespace MathLibDLL.Interfaces
+{
+    /// <summary>
+    /// Математическое ядро библиотеки - отвечает за основные операции
+    /// </summary>
+    public interface IMathCore
+    {
+        /// <summary>
+        /// Список пользовательских функций которые могут быть использованы в математическом выражении
+        /// </summary>
+        List<IFunction> UserFunctions { get; }
+
+        /// <summary>
+        /// Пользовательские константы, используемые при вычислении выражений
+        /// </summary>
+        List<Constant> UserConstants { get; }
+
+        /// <summary>
+        /// Вычислить значение выражения
+        /// </summary>
+        /// <param name="expression">Выражение</param>
+        /// <returns>Полученное значение</returns>
+        double Calculate(IExpression expression);
+
+        /// <summary>
+        /// Проверить правильность выражения
+        /// </summary>
+        /// <param name="expression">Выражение</param>
+        /// <returns>Есть ошибка \ нет</returns>
+        bool CheckExpression(IExpression expression);
+
+        /// <summary>
+        /// Добавить новую пользовательскую функцию
+        /// </summary>
+        /// <param name="function">Функция</param>
+        void AddNewFunction(IFunction function);
+
+        /// <summary>
+        /// Добавить новую пользовательскую константу
+        /// </summary>
+        /// <param name="constant">Константа</param>
+        void AddNewConstant(Constant constant);
+
+        /// <summary>
+        /// Добавить новую пользовательскую константу
+        /// </summary>
+        /// <param name="name">Имя константы</param>
+        /// <param name="value">Значение константы</param>
+        void AddNewConstant(string name, double value);
+    }
+}
