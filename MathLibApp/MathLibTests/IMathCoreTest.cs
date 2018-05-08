5e10d84479ae38f3f394f0d64ed70be9a68151df
DemiK0r <demikor97@gmail.com>
2018-05-09 01:22:43 +0300

    Из xml убрал умножение(Нужно в другой ветке делать) + добавил тесты на выход за границы +дописал все тесты

--- MathLibApp/MathLibTests/IMathCoreTest.cs
+++ MathLibApp/MathLibTests/IMathCoreTest.cs
@@ -1,46 +1,57 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MathLibDLL.Interfaces;
-using MathLibDLL.Models;
 using System.Collections.Generic;
 
 namespace MathLibTests
 {
+
     [TestClass]
     public class IMathCoreTest
     {
         public TestContext TestContext { get; set; }
+        IMathCore IMC;
+        IExpression IE;
 
-
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML","textData.xml", "Calculate",
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML","testData.xml", "Calculate",
             DataAccessMethod.Sequential)]
         [TestMethod]
         public void MathLib_Calculate_FromXML()
         {
             //arrange
             string data = Convert.ToString(TestContext.DataRow["data"]);
+            double expected = double.NaN;
             //acts
-
+            IE.Expression = data;
+            double actual = IMC.Calculate(IE);
             //assert
-
+            Assert.AreEqual(actual, expected);
         }
 
+        [TestMethod]
         public void Calculate_MaxValue_Plus_1_Returned_Null()
         {
             //arrange
             double mv = double.MaxValue;
+            double expected = double.NaN;
             //acts
-
+            IE.Expression = mv+"+1";
+            double actual = IMC.Calculate(IE);
             //assert
+            Assert.AreEqual(actual, expected);
         }
 
+        [TestMethod]
         public void Calculate_MinValue_Minus_1_Returned_Null()
         {
             //arrange
             double mv = double.MinValue;
+            double expected = double.NaN;
             //acts
-
+            IE.Expression ="-" + mv + "-1";
+            double actual = IMC.Calculate(IE);
             //assert
+            Assert.AreEqual(actual, expected);
         }
     }
 }
