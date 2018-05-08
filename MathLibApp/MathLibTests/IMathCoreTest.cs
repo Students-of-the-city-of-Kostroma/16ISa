83af027a2eed3adf90074b75e2035477907060fa
DemiK0r <demikor97@gmail.com>
2018-05-08 10:15:08 +0300

    Добавил Xml-файл с данными для теста

---
+++ MathLibApp/MathLibTests/IMathCoreTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLibDLL;
+
+namespace MathLibTests
+{
+    [TestClass]
+    public class IMathCoreTest
+    {
+        [ClassInitialize]
+        public void ClassInitialize()
+        {
+
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML","textData.xml", "Calculate",
+            DataAccessMethod.Sequentail)]
+        [TestMethod]
+        public void MathLib_Calculate_FromXML()
+        {
+            //arrange
+            string data = Convert.ToString(TextContext.DataRow["data"]);
+            //acts
+            
+            //assert
+
+        }
+    }
+}
