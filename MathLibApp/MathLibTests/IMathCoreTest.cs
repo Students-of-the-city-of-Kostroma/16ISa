d6b0b8fa3a360a20e675d1d581d271f772311741
DemiK0r <demikor97@gmail.com>
2018-05-09 01:33:49 +0300

    Перекинул тесты с ветки Task#27 с умножением и делением сюда + написал базу для этого теста

---
+++ MathLibApp/MathLibTests/IMathCoreTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLibDLL.Interfaces;
+
+namespace MathLibTests
+{
+    [TestClass]
+    public class IMathCoreTest
+    {
+        public TestContext TestContext { get; set; }
+        IMathCore IMC;
+        IExpression IE;
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "testData.xml",
+            "Calculate", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void Calculate_From_XML()
+        {
+            //arrange
+            string data = Convert.ToString(TestContext.DataRow["data"]);
+            double expected = double.NaN;//предлагается использовать null, 
+            //но его нельзя поместить в тип double
+            //acts
+            IE.Expression = data;
+            double actual = IMC.Calculate(IE);//якобы на выходе будет null            
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
