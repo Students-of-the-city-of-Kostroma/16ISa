a4ba7ed4ca9824d01192320936dd45c7792a4553
Leonid Gorev <studiesgorev@gmail.com>
2018-05-09 12:29:20 +0300

    leonidFure MultiplicationDivisionCalc_1 MultiplicationDivisionCalc_2 MultiplicationDivisionCalc_3 MultiplicationDivisionCalc_4

--- MathLibApp/MathLibTests/MathCoreTest.cs
+++ MathLibApp/MathLibTests/MathCoreTest.cs
@@ -26,5 +26,52 @@ namespace MathLibTests
             //assert
             //Assert.AreEqual(actual, expected);
         }
+        [TestMethod]
+        public void MultiplicationDivisionCalc_1()
+        {
+            //arrange
+            double expected = 1;
+            string data = "1*2/2";
+            //acts
+            //double actual = mc.Calculate(data);
+            //assert
+            //Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultiplicationDivisionCalc_2()
+        {
+            //arrange
+            double expected = 0.5;
+            string data = "1/20.*10";
+            //acts
+            //double actual = mc.Calculate(data);
+            //assert
+            //Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultiplicationDivisionCalc_3()
+        {
+            //arrange
+            double expected = 2.10327613;
+            string data = "52.5*321/8012.5";
+            //acts
+            //double actual = mc.Calculate(data);
+            //assert
+            //Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultiplicationDivisionCalc_4()
+        {
+            //arrange
+            double expected = 856356.822;
+            string data = "8911.52*219.48/23/34*786/2012.5*0.5*2999/502*10500.228/322*9";
+            //acts
+            //double actual = mc.Calculate(data);
+            //assert
+            //Assert.AreEqual(expected, actual);
+        }
     }
 }
