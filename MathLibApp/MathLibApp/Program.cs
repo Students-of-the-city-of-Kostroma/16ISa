f95943b1e0bca2b2b9e99b54da33823fcf72228d
Павел <pavelkalashnikov9819@gmail.com>
2018-05-10 20:52:34 +0300

    консольное приложение для пользования библиотекой

--- MathLibApp/MathLibApp/Program.cs
+++ MathLibApp/MathLibApp/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathLibDLL.Interfaces;
+using MathLibDLL.Models;
+using MathLibDLL;
 
 namespace MathLibApp
 {
@@ -10,6 +13,19 @@ namespace MathLibApp
     {
         static void Main(string[] args)
         {
+            string str = null;
+            str = Console.ReadLine();
+            if (str.Length > 50)
+            {
+                Console.WriteLine("Ошибка введено более 50 символов");
+            }
+            else
+            {
+                MathCore a = new MathCore();
+                a.Calculate(str);
+
+
+            }
         }
     }
 }
