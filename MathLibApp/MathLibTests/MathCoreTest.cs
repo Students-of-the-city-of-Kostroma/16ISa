764114c89c5e2ee2c27f96277e44c15ca2678d0f
Leonid Gorev <studiesgorev@gmail.com>
2018-05-20 13:32:58 +0300

    Написаны тесты: CalcPowerOf_1 CalcPowerOf_2 CalcPowerOf_3 CalcPowerOf_4 CalcPowerOf_5 CalcPowerOf_6

--- MathLibApp/MathLibTests/MathCoreTest.cs
+++ MathLibApp/MathLibTests/MathCoreTest.cs
@@ -214,5 +214,72 @@ namespace MathLibTests
             //assert
             Assert.AreEqual(actual, expected);
         }
+        [TestMethod]
+        public void CalcPowerOf_1()
+        {
+            //arrange
+            string data = "2^5";
+            double expected = 32;
+            //acts
+            double actual = mc.Calculate(data);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void CalcPowerOf_2()
+        {
+            //arrange
+            string data = "2^-5";
+            double expected = 0.03125;
+            //acts
+            double actual = mc.Calculate(data);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void CalcPowerOf_3()
+        {
+            //arrange
+            string data = "2^(2*10/4)";
+            double expected = 32;
+            //acts
+            double actual = mc.Calculate(data);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void CalcPowerOf_4()
+        {
+            //arrange
+            string data = "2^2^3";
+            double expected = 256;
+            //acts
+            double actual = mc.Calculate(data);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void CalcPowerOf_5()
+        {
+            //arrange
+            string data = "5^0.25";
+            double expected = 1.49534878;
+            //acts
+            double actual = mc.Calculate(data);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void CalcPowerOf_6()
+        {
+            //arrange
+            string data = "(2*10-5)^2";
+            double expected = 225;
+            //acts
+            double actual = mc.Calculate(data);
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
     }
 }
